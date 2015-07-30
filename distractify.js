2c3783e94d97716ea5fd6110a63d593188904ef2
Laura Knight <ljknight@gmail.com>
2015-07-30 02:02:55 -0700

    Add createRandomTagline function; update list of tags

--- distractify.js
+++ distractify.js
@@ -1,13 +1,24 @@
+var $tagline = $('.tagline');
+var randomTagline;
 var $beginButton = $('.begin-button');
 var $startOverButton = $('.startover-button');
 var $randomButton = $('.random-button');
-var $nav = $('nav');
+var $tagContainer = $('.tag-container');
 var $content = $('.content');
 var $contentImage = $('.content-image');
 var $loadingImage = $('.loading-image');
 var randomTag;
 
-var tagList = ['funny', 'cute-gif', 'lol', 'dog', 'dog-gif', 'cat', 'cat-gif', 'dog-gif', 'lol-gif', 'animals'];
+
+var createRandomTagline = function() {
+  var taglineList = ['For the brain-dead human.', 'Weird gifs courtesy of Tumblr.', 'Who knew there were so many gifs on the Internet?', 'Powered by pizza. Running on no sleep.']
+  randomTagline = taglineList[Math.floor(Math.random() * taglineList.length)];
+  $tagline.text(randomTagline);
+};
+
+createRandomTagline();
+
+var tagList = ['funny-gif', 'lol-gif', 'dog', 'dog-gif', 'cat-gif', 'animals', 'animal-gif', 'puppy', 'puppy-gif', 'kitten', 'kitten-gif', 'corgi', 'space-gif', 'goat-gif', 'cute-overload', 'kawaii-gif', 'rilakkuma-gif', 'pusheen', 'spongebob-gif', 'pinterest-fail', 'harry-potter-gif', 'mesmerizing-gif', 'pizza-gif', 'arrested-development-gif', 'donut-gif'];
 
 var timestamp = [];
 var currentMs = Math.floor((new Date()).getTime()/1000);
@@ -26,7 +37,7 @@ var loadRandomImage = function(tag) {
   var randomTime = timestamp[Math.floor(Math.random() * timestamp.length)];
   console.log('random time: ', randomTime);
 
-  $nav.append($('<button>'+tag+'</button>').addClass(tag + ' tagged'));
+  $tagContainer.append($('<button>'+tag+'</button>').addClass(tag + ' tagged'));
   $tagged = $('.tagged');
   
   $loadingImage.show();
@@ -60,7 +71,7 @@ var loadRandomImage = function(tag) {
     var tagClass = $(this).closest('button').attr('class').split(' ')[0];
     loadRandomImage(tagClass);
   });
-}
+};
 
 $beginButton.click(function() {
   $startOverButton.removeClass('hide');
@@ -75,12 +86,16 @@ $beginButton.click(function() {
 $randomButton.click(function() {
   createRandomTag();
   loadRandomImage(randomTag);
+  createRandomTagline();
+
 });
 
 $startOverButton.click(function() {
   $contentImage.attr('src', '');
-  $nav.html('');
+  $tagContainer.html('');
+  createRandomTagline();
   $startOverButton.addClass('hide');
+  $randomButton.addClass('hide');
   $beginButton.removeClass('hide');
 });
 
