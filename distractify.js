37c6aab5032edb0d39a6157cba3c30955689fe25
Laura Knight <ljknight@gmail.com>
2015-07-30 17:22:53 -0700

    Limit bubbles to 10; bounce on each 'random' button click

--- distractify.js
+++ distractify.js
@@ -4,22 +4,26 @@ var $beginButton = $('.begin-button');
 var $startOverButton = $('.startover-button');
 var $randomButton = $('.random-button');
 var $tagContainer = $('.tag-container');
+var $tagged; 
 var $content = $('.content');
 var $contentImage = $('.content-image');
 var $loadingImage = $('.loading-image');
 var randomTag;
+var tagCount = 1;
 
-
+// adds tagline to header
 var createRandomTagline = function() {
-  var taglineList = ['For the brain-dead human.', 'Weird gifs courtesy of Tumblr.', 'Who knew there were so many gifs on the Internet?', 'Powered by pizza. Running on no sleep.']
+  var taglineList = ['For the brain-dead human.', 'Weird things courtesy of Tumblr.', 'Who knew there were so many gifs on the Internet?', 'Powered by pizza.']
   randomTagline = taglineList[Math.floor(Math.random() * taglineList.length)];
   $tagline.text(randomTagline);
 };
 
 createRandomTagline();
 
-var tagList = ['funny-gif', 'lol-gif', 'dog', 'dog-gif', 'cat-gif', 'animals', 'animal-gif', 'puppy', 'puppy-gif', 'kitten', 'kitten-gif', 'corgi', 'space-gif', 'goat-gif', 'cute-overload', 'kawaii-gif', 'rilakkuma-gif', 'pusheen', 'spongebob-gif', 'pinterest-fail', 'harry-potter-gif', 'mesmerizing-gif', 'pizza-gif', 'arrested-development-gif', 'donut-gif'];
+// tags for photos
+var tagList = ['funny-gif', 'lol-gif', 'dog', 'dog-gif', 'cat-gif', 'animals', 'animal-gif', 'puppy', 'puppy-gif', 'kitten', 'kitten-gif', 'corgi', 'space-gif', 'goat-gif', 'cute-overload', 'kawaii-gif', 'rilakkuma-gif', 'pusheen', 'spongebob-gif', 'pinterest-fail', 'harry-potter-gif', 'mesmerizing-gif', 'pizza-gif', 'arrested-development-gif', 'donut-gif', '30-rock-gif', 'tina-fey-gif'];
 
+// creates range of timestamps from which to pull posts from - increase variety
 var timestamp = [];
 var currentMs = Math.floor((new Date()).getTime()/1000);
   for (var i = 1300000000; i < currentMs; i += 1000000) {
@@ -37,9 +41,25 @@ var loadRandomImage = function(tag) {
   var randomTime = timestamp[Math.floor(Math.random() * timestamp.length)];
   console.log('random time: ', randomTime);
 
-  $tagContainer.append($('<button>'+tag+'</button>').addClass(tag + ' tagged'));
+  //$tagContainer.append($('<button>'+tag+'</button>').addClass(tag + ' tagged'));
   $tagged = $('.tagged');
-  
+
+  // max 10 tag bubbles & maintain bouncing animation
+  if (tagCount === 1) {
+    $('.10').removeClass('animated');
+    $('.'+(tagCount-1)+'').removeClass('animated');
+    $('.'+tagCount+'').text(tag).removeClass('hide').addClass(''+tag+' animated');
+    tagCount++
+  } else if (tagCount < 10) {
+    $('.'+(tagCount-1)+'').removeClass('animated');
+    $('.'+tagCount+'').text(tag).removeClass('hide').addClass(''+tag+' animated');
+    tagCount++
+  } else if (tagCount === 10) {
+    $('.'+(tagCount-1)+'').removeClass('animated');
+    $('.10').text(tag).removeClass('hide').addClass(''+tag+' animated');
+    tagCount = 1;
+  }
+
   $loadingImage.show();
   $.ajax({
     url: 'http://api.tumblr.com/v2/tagged?tag='+tag+'&before='+randomTime+'&api_key=dlqISFs67gOticMc5ReDbzBDSAAkvVlnzIepQ42qhwD7m1rtYp', 
@@ -68,7 +88,7 @@ var loadRandomImage = function(tag) {
 
   // load image with selected tag
   $tagged.click(function() {
-    var tagClass = $(this).closest('button').attr('class').split(' ')[0];
+    var tagClass = $(this).closest('button').attr('class').split(' ')[3];
     loadRandomImage(tagClass);
   });
 };
@@ -78,25 +98,24 @@ $beginButton.click(function() {
   $randomButton.removeClass('hide');
   $beginButton.addClass('hide');
 
+  tagCount = 1;
   createRandomTag();
   loadRandomImage(randomTag);
-
 });
 
 $randomButton.click(function() {
   createRandomTag();
   loadRandomImage(randomTag);
   createRandomTagline();
-
 });
 
 $startOverButton.click(function() {
   $contentImage.attr('src', '');
-  $tagContainer.html('');
   createRandomTagline();
   $startOverButton.addClass('hide');
   $randomButton.addClass('hide');
   $beginButton.removeClass('hide');
+  $tagged.addClass('hide');
 });
 
 
